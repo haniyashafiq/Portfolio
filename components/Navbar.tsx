bfec2e59d46dce256c06159997216ac7956b3fb9
haniyashafiq <haniyashafiq2005@gmail.com>
2025-11-23 22:49:15 +0500

    removed menu icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -116,24 +116,7 @@ export default function Navbar() {
 
           <div className="flex items-center space-x-4">
             <ThemeToggle />
-            <motion.button
-              className="md:hidden p-2 text-charcoal dark:text-cool-gray"
-              whileTap={{ scale: 0.9 }}
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </motion.button>
+            
           </div>
         </div>
       </div>
